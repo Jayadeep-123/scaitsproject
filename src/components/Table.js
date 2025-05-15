b426582702f8614ecfcb70a5b32d2187e83ed4d2
jayadeeppasupuleti <jayadeeppasupuleti7731@gmail.com>
2025-05-15 17:26:12 +0530

    some Message

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import '../styles/Table.css';
 import { Link } from 'react-router-dom';
 
@@ -30,6 +30,28 @@ function DashBoard() {
 
 
   ]);  
+ 
+  const [isVisible, setIsVisible] = useState(true);
+  let lastScrollY = window.scrollY;
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+
+      if (currentScrollY > lastScrollY && currentScrollY > 50) {
+        // Scrolling down
+        setIsVisible(false);
+      } else {
+        // Scrolling up
+        setIsVisible(true);
+      }
+      lastScrollY = currentScrollY;
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   
   const [currentPage, setCurrentPage] = useState(1); 
   
@@ -45,6 +67,7 @@ function DashBoard() {
     }
   };
   
+  
 
   return (
     <div className="dashboard-container">
@@ -53,7 +76,7 @@ function DashBoard() {
       <h1>Fee Payment Year</h1>
     </div>   
     <div className="header-right">
-    <div className="buttons">
+   <div className={`buttons ${isVisible ? 'slide-in' : 'slide-out'}`}>
           <button className="filter">
             <span className="icon"><svg width="14" height="12" viewBox="0 0 14 16" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path fill-rule="evenodd" clip-rule="evenodd" d="M0 1C0 0.734784 0.105357 0.48043 0.292893 0.292893C0.48043 0.105357 0.734784 0 1 0H13C13.2652 0 13.5196 0.105357 13.7071 0.292893C13.8946 0.48043 14 0.734784 14 1V4C13.9999 4.26519 13.8946 4.51951 13.707 4.707L9 9.414V13C8.99994 13.2652 8.89455 13.5195 8.707 13.707L6.707 15.707C6.56715 15.8468 6.38898 15.942 6.19503 15.9806C6.00108 16.0192 5.80005 15.9993 5.61735 15.9237C5.43465 15.848 5.27848 15.7199 5.1686 15.5555C5.05871 15.391 5.00004 15.1978 5 15V9.414L0.293 4.707C0.105451 4.51951 5.66374e-05 4.26519 0 4V1Z" fill="#5B6871"/>
